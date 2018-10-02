3b63d2defc304193eb03ed58ebf8f57fc0609ac2
Hoàng <Huyhoang.ttw@gmail.com>
2018-10-02 12:21:21 +0700

    update

--- Dictionary.java
+++ Dictionary.java
@@ -10,24 +10,26 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 public class Dictionary extends JFrame implements WindowListener{
 	private JButton search = new JButton ("Search");
-	private JFrame mainFrame; 
-   	private JList listWord = new JList();
+	private JFrame mainFrame ; 
    	private JLabel headerLabel;	//   private JLabel statusLabel;
+   	private JLabel face = new JLabel(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\image.jpeg"));
    	private JPanel controlPanel;  
    	protected JTextField SearchWord = new JTextField(70);	// ô nhập từ cần tra
    	protected JTextArea translate = new JTextArea(100,100); 	// nghĩa của từ đó
-   	ImageIcon imageSpeak = new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\speak.png");
-   	ImageIcon imageFix = new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\fix.png");
-	private JButton speak = new JButton(imageSpeak);
-	private JButton fix = new JButton(imageFix);
-   
+	private JButton speak = new JButton(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\speak.png"));
+	private JButton fix = new JButton(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\fix.png"));
+	private JMenuBar file = new JMenuBar();
+	private JButton save = new JButton (new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\save.png"));
+	
 	public  Dictionary(){
 		prepareGUI();
 	}  
@@ -46,25 +48,26 @@ public class Dictionary extends JFrame implements WindowListener{
 		Dimension mainSize = new Dimension(700,450);
 		controlPanel.setPreferredSize(mainSize);
 		JScrollPane scrollPane = new JScrollPane(translate);
-		translate.setBounds (200,60,490,385);
+		translate.setBounds (200,60,494,387);
 		translate.setBackground(Color.white);
 		SearchWord.setBounds(300,5,290,25);
 		search.setBounds(592, 5, 100, 24);
 		speak.setBounds(200,30,35,30);
 		fix.setBounds(240,30,35,30);
-		listWord.setBounds(7, 35, 180, 410);
-	}
+		save.setBounds(280,30,35,30);
+}
    
 	private void showDictionary(){
 		mainFrame.setResizable(false);
-		mainFrame.add(listWord);
 	    mainFrame.add(search);
 	    mainFrame.add(speak);
 	    mainFrame.add(fix);
+	    mainFrame.add(save);
 	    mainFrame.add(SearchWord);
 		mainFrame.add(translate);
 	    mainFrame.add(headerLabel);
 	    mainFrame.add(controlPanel);
+	    mainFrame.add(face);
 	    mainFrame.pack();
 	    mainFrame.setVisible(true);  
 	    mainFrame.addWindowListener(new WindowAdapter() {
