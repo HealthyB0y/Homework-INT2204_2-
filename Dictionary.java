27b0d91e4e7bc9e34236c765384d7ac1e79d0237
Bùi Huy Hoàng <40866207+HealthyB0y@users.noreply.github.com>
2018-10-02 15:51:29 +0700

    update

--- Dictionary.java
+++ Dictionary.java
@@ -2,6 +2,8 @@ package BigHomework;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -10,55 +12,69 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-public class Dictionary extends JFrame implements WindowListener{
-	private JButton search = new JButton ("Search");
+public class Dictionary extends JFrame implements ActionListener{
+	
+	private JButton search;
 	private JFrame mainFrame ; 
-   	private JLabel headerLabel;	//   private JLabel statusLabel;
-   	private JLabel face = new JLabel(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\image.jpeg"));
+   	private JLabel headerLabel;
    	private JPanel controlPanel;  
-   	protected JTextField SearchWord = new JTextField(70);	// ô nhập từ cần tra
-   	protected JTextArea translate = new JTextArea(100,100); 	// nghĩa của từ đó
+	private JMenuBar file = new JMenuBar();
+   	protected JTextField SearchWord = new JTextField(70);
+   	protected JTextArea translate = new JTextArea(100,100); 
 	private JButton speak = new JButton(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\speak.png"));
 	private JButton fix = new JButton(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\fix.png"));
-	private JMenuBar file = new JMenuBar();
 	private JButton save = new JButton (new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\save.png"));
+   	private JLabel face = new JLabel(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\image.jpeg"));
 	
-	public  Dictionary(){
+   	public  Dictionary(){
 		prepareGUI();
 	}  
-   
+	
 	public static void main(String[] args){
 		Dictionary Dictionary = new Dictionary();        
 		Dictionary.showDictionary();
 	}   
    
 	private void prepareGUI(){
+		search = new JButton ("Search");
+		search.setBounds(592, 5, 100, 24);
+		
 		mainFrame = new JFrame("Dictionary");
 		mainFrame.setSize(500,500);
+		
 		headerLabel = new JLabel("",JLabel.CENTER ); 
 		controlPanel = new JPanel();
 		controlPanel.setLayout(new FlowLayout());
 		Dimension mainSize = new Dimension(700,450);
 		controlPanel.setPreferredSize(mainSize);
+		
 		JScrollPane scrollPane = new JScrollPane(translate);
+		translate.setEditable(false);
 		translate.setBounds (200,60,494,387);
 		translate.setBackground(Color.white);
+		
+		
 		SearchWord.setBounds(300,5,290,25);
-		search.setBounds(592, 5, 100, 24);
+		SearchWord.setEditable(true);
+		
+		
 		speak.setBounds(200,30,35,30);
+		
+		
 		fix.setBounds(240,30,35,30);
+		fix.addActionListener( this);
+		
+		
 		save.setBounds(280,30,35,30);
-}
+		save.addActionListener( this);
+	}
    
-	private void showDictionary(){
-		mainFrame.setResizable(false);
+	private void showDictionary(){	
 	    mainFrame.add(search);
 	    mainFrame.add(speak);
 	    mainFrame.add(fix);
@@ -69,6 +85,7 @@ public class Dictionary extends JFrame implements WindowListener{
 	    mainFrame.add(controlPanel);
 	    mainFrame.add(face);
 	    mainFrame.pack();
+	    mainFrame.setResizable(false);
 	    mainFrame.setVisible(true);  
 	    mainFrame.addWindowListener(new WindowAdapter() {
 	    public void windowClosing(WindowEvent windowEvent){
@@ -76,32 +93,18 @@ public class Dictionary extends JFrame implements WindowListener{
 	    }        
 	    });
 	}
-@Override
-	public void windowActivated(WindowEvent e) {
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if(e.getSource() == fix) {
+			SearchWord.setEditable(false);
+			translate.setEditable(true);
+		}
+		if(e.getSource() == save) {
+			SearchWord.setEditable(true);
+			translate.setEditable(false);
+		}
+		
 	}
-@Override
-	public void windowClosed(WindowEvent e) {
-		// TODO Auto-generated method stub
-	}
-@Override
-	public void windowClosing(WindowEvent e) {
-		// TODO Auto-generated method stub	
-	}
-@Override
-	public void windowDeactivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-	}
-@Override
-	public void windowDeiconified(WindowEvent e) {
-		// TODO Auto-generated method stub	
-	}
-@Override
-	public void windowIconified(WindowEvent e) {
-		// TODO Auto-generated method stub	
-	}
-@Override
-	public void windowOpened(WindowEvent e) {
-		// TODO Auto-generated method stub
-	}	
 }
