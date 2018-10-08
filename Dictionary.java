83cdac15e1157194f8f65ce570f11eb463f9bf46
Bùi Huy Hoàng <40866207+HealthyB0y@users.noreply.github.com>
2018-10-08 23:15:34 +0700

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -6,13 +6,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -23,13 +24,21 @@ public class Dictionary extends JFrame implements ActionListener{
 	private JFrame mainFrame ; 
    	private JLabel headerLabel;
    	private JPanel controlPanel;  
-	private JMenuBar file = new JMenuBar();
+	private JMenu file,help;
+	private JMenuBar list = new JMenuBar();
+	private JMenuItem addWord = new JMenuItem("Add Word"),
+			history = new JMenuItem("History"),
+			exit = new JMenuItem("Exit");
    	protected JTextField SearchWord = new JTextField(70);
    	protected JTextArea translate = new JTextArea(100,100); 
-	private JButton speak = new JButton(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\speak.png"));
-	private JButton fix = new JButton(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\fix.png"));
-	private JButton save = new JButton (new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\save.png"));
-   	private JLabel face = new JLabel(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\image.jpeg"));
+	private JButton speak = 
+			new JButton(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\speak.png"));
+	private JButton fix = 
+			new JButton(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\fix.png"));
+	private JButton save = 
+			new JButton (new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\save.png"));
+   	private JLabel face = 
+   			new JLabel(new ImageIcon("C:\\Users\\Huy Hoang\\eclipse-workspace\\java-traning\\src\\BigHomework\\image.jpeg"));
 	
    	public  Dictionary(){
 		prepareGUI();
@@ -42,7 +51,8 @@ public class Dictionary extends JFrame implements ActionListener{
    
 	private void prepareGUI(){
 		search = new JButton ("Search");
-		search.setBounds(592, 5, 100, 24);
+		search.setBounds(350, 5, 100, 26);
+		search.addActionListener(this);
 		
 		mainFrame = new JFrame("Dictionary");
 		mainFrame.setSize(500,500);
@@ -55,23 +65,33 @@ public class Dictionary extends JFrame implements ActionListener{
 		
 		JScrollPane scrollPane = new JScrollPane(translate);
 		translate.setEditable(false);
-		translate.setBounds (200,60,494,387);
+		translate.setBounds (200,60,500,387);
 		translate.setBackground(Color.white);
 		
 		
-		SearchWord.setBounds(300,5,290,25);
+		SearchWord.setBounds(15,5,330,25);
 		SearchWord.setEditable(true);
 		
 		
-		speak.setBounds(200,30,35,30);
+		speak.setBounds(200,30,35,28);
+		speak.addActionListener(this);
 		
 		
-		fix.setBounds(240,30,35,30);
+		fix.setBounds(240,30,35,28);
 		fix.addActionListener( this);
 		
 		
-		save.setBounds(280,30,35,30);
+		save.setBounds(280,30,35,28);
 		save.addActionListener( this);
+		
+		file = new JMenu("File");
+		help = new JMenu("Help");
+		file.add(addWord);
+		file.add(history);
+		file.add(exit);
+		list.add(file);
+		list.add(help);
+		
 	}
    
 	private void showDictionary(){	
@@ -85,6 +105,7 @@ public class Dictionary extends JFrame implements ActionListener{
 	    mainFrame.add(controlPanel);
 	    mainFrame.add(face);
 	    mainFrame.pack();
+	    mainFrame.setJMenuBar(list);
 	    mainFrame.setResizable(false);
 	    mainFrame.setVisible(true);  
 	    mainFrame.addWindowListener(new WindowAdapter() {
